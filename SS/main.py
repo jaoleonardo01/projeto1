1ccab8fba0a7f579c2c8e95f364fca2e14ab0b41
jo053066 <jao@oldschool.not.br>
2021-03-24 15:52:10 -0300

    commit message

--- SS/main.py
+++ SS/main.py
@@ -1,3 +1,7 @@
+import sys
+import subprocess
+import os
+import commands
 from transmissor_SA import *
 from transmissor_SR import *
 from receptor_SA import *
@@ -6,8 +10,9 @@ from time import sleep
 
 def main():
 
+	ip = commands.getoutput("hostname -i")
 	tSA = TransmissorSA("172.31.11.228")
-	mensagem = "Ronaldo!"
+	mensagem = str(ip)
 	tSA.enviar(mensagem)
 
 if __name__ == '__main__':
